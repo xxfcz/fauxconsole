07d384b6563ad00779f6b6ec6257c155ad0db34c
xxfcz <xxf_cz@163.com>
2017-03-16 14:06:47 +0800

    set up window.console only after DOM is ready

--- fauxconsole.js
+++ fauxconsole.js
@@ -3,7 +3,10 @@
  * re-written by Roland Hummel (https://github.com/defaude/fauxconsole).
  */
 (function (window) {
-    "use strict"; // mostly for me and my IDE :)
+    // for use of callee in IEContentLoaded()
+    // "use strict"; // mostly for me and my IDE :)
+
+    var consoleName = 'console';   // use a name other than 'console' for the convenience of debugging
 
     // --- some tools ---
     function createElement(tagName, attributes, innerHTML) {
@@ -19,79 +22,142 @@
         return element;
     }
 
-    var consoleDiv, consoleContent;
-    
-    //check existence of console
-    if (typeof window.console === 'undefined' || typeof window.console.log === 'undefined') {
-        consoleDiv = createElement('div', { className: 'fauxconsole' });
+    /*!
+     * contentloaded.js
+     *
+     * Author: Diego Perini (diego.perini at gmail.com)
+     * Summary: cross-browser wrapper for DOMContentLoaded
+     * Updated: 20101020
+     * License: MIT
+     * Version: 1.2
+     *
+     * URL:
+     * http://javascript.nwbox.com/ContentLoaded/
+     * http://javascript.nwbox.com/ContentLoaded/MIT-LICENSE
+     *
+     */
 
-        consoleDiv.appendChild(createElement('a', { href: 'javascript:console.hide()' }, 'hide'));
-        consoleDiv.appendChild(createElement('a', { href: 'javascript:console.show()' }, 'show'));
-        consoleDiv.appendChild(createElement('a', { href: 'javascript:console.clear()' }, 'clear'));
+    // @win window reference
+    // @fn function reference
+    function contentLoaded(win, fn) {
 
-        consoleContent = createElement('pre');
-        consoleDiv.appendChild(consoleContent);
+        var done = false, top = true,
 
-        (document.body || document.documentElement).appendChild(consoleDiv);
-        hide();
+            doc = win.document, root = doc.documentElement,
 
-        window.console = {
-            show: function show() { 
-                consoleDiv.style.display = 'block'; 
+            add = doc.addEventListener ? 'addEventListener' : 'attachEvent',
+            rem = doc.addEventListener ? 'removeEventListener' : 'detachEvent',
+            pre = doc.addEventListener ? '' : 'on',
+
+            init = function (e) {
+                if (e.type == 'readystatechange' && doc.readyState != 'complete') return;
+                (e.type == 'load' ? win : doc)[rem](pre + e.type, init, false);
+                if (!done && (done = true)) fn.call(win, e.type || e);
             },
-            hide: function hide() { 
-                consoleDiv.style.display = 'none'; 
-            },
-            clear: function clear () {
-                consoleContent.innerHTML = '';
-            },
-            log: function log() {
-                for (var i = 0, l = arguments.length; i < l; ++i) {
-                    consoleContent.innerHTML += '<br/><br/>' + arguments[i];
+
+            poll = function () {
+                try {
+                    root.doScroll('left');
+                } catch (e) {
+                    setTimeout(poll, 50);
+                    return;
                 }
-                show();
-            }
-            
-        };
-    }
-    
-    if (typeof window.console.debug === 'undefined') {
-        window.console.debug = window.console.log;
-    }
-    
-    if (typeof window.console.info === 'undefined') {
-        window.console.info = window.console.log;
-    }
-    
-    if (typeof window.console.warn === 'undefined') {
-        window.console.warn = window.console.log;
-    }
-    
-    if (typeof window.console.error === 'undefined') {
-        window.console.error = window.console.log;
-    }
-    
-    if (typeof window.console.assert === 'undefined') {
-        window.console.assert = function assert() {
-            if (!arguments[0]) {
-                if (arguments[1]) {
-                    window.console.log(arguments[1]);
-                } else {
-                    window.console.log('Assertion failed');
+                init('poll');
+            };
+
+        if (doc.readyState == 'complete') fn.call(win, 'lazy');
+        else {
+            if (doc.createEventObject && root.doScroll) {
+                try {
+                    top = !win.frameElement;
+                } catch (e) {
                 }
+                if (top) poll();
             }
-        };
+            doc[add](pre + 'DOMContentLoaded', init, false);
+            doc[add](pre + 'readystatechange', init, false);
+            win[add](pre + 'load', init, false);
+        }
+
     }
-    
-    if (typeof window.console.show === 'undefined') {
-        window.console.show = function () {};
+
+    function setup() {
+        var consoleDiv, consoleContent;
+
+        //check existence of console
+        if (typeof window[consoleName] === 'undefined' || typeof window[consoleName].log === 'undefined') {
+            consoleDiv = createElement('div', { className: 'fauxconsole' });
+
+            consoleDiv.appendChild(createElement('a', { href: 'javascript:' + consoleName + '.hide()' }, 'hide'));
+            consoleDiv.appendChild(createElement('a', { href: 'javascript:' + consoleName + '.show()' }, 'show'));
+            consoleDiv.appendChild(createElement('a', { href: 'javascript:' + consoleName + '.clear()' }, 'clear'));
+
+            consoleContent = createElement('pre');
+            consoleDiv.appendChild(consoleContent);
+
+            (document.body || document.documentElement).appendChild(consoleDiv);
+            // hide();
+
+            window[consoleName] = {
+                show: function show() {
+                    consoleDiv.style.display = 'block';
+                },
+                hide: function hide() {
+                    consoleDiv.style.display = 'none';
+                },
+                clear: function clear() {
+                    consoleContent.innerHTML = '';
+                },
+                log: function log() {
+                    for (var i = 0, l = arguments.length; i < l; ++i) {
+                        consoleContent.innerHTML += '<br/><br/>' + arguments[i];
+                    }
+                    this.show();
+                }
+
+            };
+        }
+
+        if (typeof window[consoleName].debug === 'undefined') {
+            window[consoleName].debug = window[consoleName].log;
+        }
+
+        if (typeof window[consoleName].info === 'undefined') {
+            window[consoleName].info = window[consoleName].log;
+        }
+
+        if (typeof window[consoleName].warn === 'undefined') {
+            window[consoleName].warn = window[consoleName].log;
+        }
+
+        if (typeof window[consoleName].error === 'undefined') {
+            window[consoleName].error = window[consoleName].log;
+        }
+
+        if (typeof window[consoleName].assert === 'undefined') {
+            window[consoleName].assert = function assert() {
+                if (!arguments[0]) {
+                    if (arguments[1]) {
+                        window[consoleName].log(arguments[1]);
+                    } else {
+                        window[consoleName].log('Assertion failed');
+                    }
+                }
+            };
+        }
+
+        if (typeof window[consoleName].show === 'undefined') {
+            window[consoleName].show = function () {};
+        }
+
+        if (typeof window[consoleName].hide === 'undefined') {
+            window[consoleName].hide = function () {};
+        }
+
+        if (typeof window[consoleName].clear === 'undefined') {
+            window[consoleName].clear = function () {};
+        }
     }
-    
-    if (typeof window.console.hide === 'undefined') {
-        window.console.hide = function () {};
-    }
-    
-    if (typeof window.console.clear === 'undefined') {
-        window.console.clear = function () {};
-    }
-}(window, window.console, 'fauxconsole'));
+
+    contentLoaded(window, setup);
+}(window));
